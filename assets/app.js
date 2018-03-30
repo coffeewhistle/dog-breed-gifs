c307a3014e4f68a0ddc7aa43af7da32c588f228e
unknown <cole.hansen@gmail.com>
2018-03-29 18:25:49 -0700

    Start adding functionality to load still and click to animate

--- assets/app.js
+++ assets/app.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
     //1. Before you can make any part of our site work, you need to create an array of strings, each one related to a topic that interests you. Save it to a variable called `topics`.
     //    * We chose animals for our theme, but you can make a list to your own liking.
     var topics = ["pug", "beagle", "german shepherd", "bulldog", "pitbull"];
+    var resultNum = 9;
 
     // 2. Your app should take the topics in this array and create buttons in your HTML.
     // * Try using a loop that appends a button for each string in the array.
@@ -22,7 +23,7 @@ $(document).ready(function () {
         $("#dogs").empty();
         var searchTerm = $(this).attr("data-name");
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-            searchTerm + "&api_key=dc6zaTOxFJmzC&limit=10";
+            searchTerm + "&api_key=dc6zaTOxFJmzC&limit=" + resultNum;
 
         $.ajax({
             url: queryURL,
@@ -31,11 +32,14 @@ $(document).ready(function () {
             console.log(response);
             var results = response.data;
 
-            for (i = 0; i < 10; i++) {
+            for (i = 0; i < resultNum; i++) {
                 var dogDiv = $("<div>");
                 var p = $("<p>").text("Rating: " + results[i].rating).addClass("my-0");
-                dogImage = $("<img>").attr("src", results[i].images.fixed_height.url);
-                dogDiv.addClass("my-3 col-4");
+                dogImage = $("<img>").attr("src", results[i].images.fixed_height_still.url);
+                dogImage.attr("data-still", response[i].images.fixed_height_still.url);
+                dogImage.attr("data-animate", response[i].images.fixed_height.url);
+                dogImage.attr("data-state", "still");
+                dogDiv.addClass("my-3 col-lg-4");
                 dogDiv.append(p);
                 dogDiv.append(dogImage);
                 $("#dogs").append(dogDiv);
